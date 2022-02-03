d39a2d17b0b8b67add167c4632053c8bf2f32ba2
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-02-03 11:57:19 +0100

    cors

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ noCors: true });
 const port = process.env.PORT || 3000;
 
-server.use(cors())
+const whitelist = [
+    'http://localhost', // add here the url when you access to your angular app
+    'http://localhost:3000',
+    'http://otherdomain.com'
+];
+
+const corsOptions = {
+    credentials: true,
+    origin: function(origin, callback) {
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: 'accept, content-type'
+};
+
+server.use(cors(corsOptions))
 server.use(middlewares);
 server.use(router);
 
