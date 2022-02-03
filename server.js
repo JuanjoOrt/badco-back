02875f316014b1c9c80485ae59c2b02c28886e8e
Juanjo Ortiz <jjortiz@ilernaonline.com>
2022-02-03 12:18:15 +0100

    a

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ readOnly: false });
+const middlewares = jsonServer.defaults({ });
 const port = process.env.PORT || 3000;
+
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
 
 server.use(middlewares);
 server.use(router);
